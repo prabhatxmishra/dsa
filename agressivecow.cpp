272d624ef6ea4e6e9369b93bbf7ade020313cf3b
prabhatxmishra <prabhatmishra2402@gmail.com>
2023-01-29 01:02:42 +0530

    done

---
+++ agressivecow.cpp
@@ -0,0 +1,47 @@
+bool ispossible(vector<int> &stalls, int k,int mid)
+{
+    int cowcount=1;
+    int lastpos=stalls[0];
+    for(int i=0;i<stalls.size();i++)
+    {
+        if(stalls[i]-lastpos>=mid)
+        {
+            cowcount++;
+            if(cowcount==k)
+            {
+                return true;
+            }
+            lastpos=stalls[i];
+        }
+    }
+    return false;
+}
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    sort(stalls.begin(),stalls.end());
+    int s=0;
+    int maxi=-1;
+      for(int i=0;i<stalls.size();i++)
+      {
+          maxi=max(maxi,stalls[i]);
+      }
+     int e=maxi;
+    int mid=s+(e-s)/2;
+    int ans=-1;
+    while(s<=e)
+    {
+        if(ispossible(stalls,k,mid))
+        {
+            ans=mid;
+            s=mid+1;  
+        }
+        else
+        {
+          e=mid-1;
+        }
+        
+        mid=s+(e-s)/2;
+    }
+    return ans;
+
+}
