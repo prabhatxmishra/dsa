490d1a0b50936f960b6979f3237a72a235696301
prabhatxmishra <prabhatmishra2402@gmail.com>
2023-02-04 17:21:09 +0530

    done

---
+++ binarysearch.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void binarysearch(int arr[], int n)
+{
+    int s=0,e=n-1;
+    int mid= s+(e-s)/2;
+    while(s<=e)
+    {
+        
+    }
+}
